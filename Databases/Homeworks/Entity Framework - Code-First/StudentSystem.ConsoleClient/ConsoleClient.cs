a2814c49c377b785c6b3b4fb9f6f23b925489680
Silviya <silweto7@abv.bg>
2016-11-01 18:35:25 +0200

    up

---
+++ Databases/Homeworks/Entity Framework - Code-First/StudentSystem.ConsoleClient/ConsoleClient.cs
@@ -0,0 +1,58 @@
+﻿namespace StudentSystem.ConsoleClient
+{
+    using System;
+    using System.Linq;
+    using StudentSystem.Data;
+
+    public class ConsoleClient
+    {
+        public static void Main()
+        {
+            // IMPORTANT: Change connection string in "StudentSystem.Data/ConnectionStrings.settings"
+
+            Console.Write("Loading...");
+
+            var forumSystemContext = new StudentSystemContext();
+            forumSystemContext.Database.Initialize(true);
+
+            PrintStudents(forumSystemContext);
+            PrintCourses(forumSystemContext);
+            PrintHomeworks(forumSystemContext);
+        }
+ 
+        private static void PrintStudents(StudentSystemContext forumSystemContext)
+        {
+            Console.WriteLine("\rStudents: ");
+            foreach (var student in forumSystemContext.Students.Include("Courses"))
+            {
+                Console.WriteLine(" - {0} -> present in {1} course(s).", student.Name, student.Courses.Count());
+            }
+        }
+ 
+        private static void PrintCourses(StudentSystemContext forumSystemContext)
+        {
+            Console.WriteLine("\nCourses: ");
+            foreach (var course in forumSystemContext.Courses.Include("Homeworks"))
+            {
+                Console.WriteLine(" - {0} -> has {1} homework(s).", course.Description, course.Homeworks.Count());
+            }
+        }
+ 
+        private static void PrintHomeworks(StudentSystemContext forumSystemContext)
+        {
+            Console.WriteLine("\nHomeworks: ");
+            foreach (var homework in forumSystemContext.Homeworks.Include("Materials").Where(h => !h.StudentId.HasValue))
+            {
+                Console.WriteLine(" - {0} ({1}) -> has {2} material(s).",
+                    homework.Content, homework.Course.Description, homework.Materials.Count());
+
+                foreach (var material in homework.Materials)
+                {
+                    Console.WriteLine("    - {0} ({2}) - {1}", material.Type, material.DownloadUrl, material.Homework.Content);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Databases/Homeworks/Entity Framework - Code-First/StudentSystem.Data/StudentSystemContext.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentSystem.Data
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using StudentSystem.Data.Migrations;
+    using StudentSystem.Models;
+
+    public class StudentSystemContext : DbContext
+    {
+        public StudentSystemContext()
+            : base(ConnectionStrings.Default.DatabaseConnectionString)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<StudentSystemContext, Configuration>());
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Homework> Homeworks { get; set; }
+
+        public DbSet<Material> Materials { get; set; }
+    }
+}
